96e933a9490ca675940fdc80abd18704bd41ca6d
milanbhatiya59 <milanbhatiya59@gmail.com>
2025-04-01 16:39:08 +0530

    modify chat messages

--- src/components/ChatBot/ChatMessages.jsx
+++ src/components/ChatBot/ChatMessages.jsx
@@ -10,11 +10,9 @@ const ChatMessage = ({ chat }) => {
     >
       {chat.role === "model" && <ChatbotIcon />}
       <p
-        className={`px-4 py-2 rounded-lg max-w-[75%] break-words text-sm ${
-          chat.role === "model"
-            ? "bg-primary text-white dark:bg-primary-dark"
-            : "bg-gray-200 text-gray-900 dark:bg-gray-700 dark:text-white"
-        } ${chat.isError ? "bg-red-500 text-white" : ""}`}
+        className={`px-4 py-2 max-w-[75%] break-words text-sm bg-gray-200 dark:bg-gray-700 text-gray-900 dark:text-gray-100 p-3 rounded-lg ${
+          chat.isError ? "bg-red-500 text-black dark:text-white" : ""
+        }`}
       >
         {chat.text}
       </p>
